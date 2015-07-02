17006c339f52c8a55c59b56cb43b3dc20af0e4e2
szamani <szchem20@gmail.com>
2015-07-02 16:11:51 +0430

    remove some unnecessary imports

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,19 +1,7 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
